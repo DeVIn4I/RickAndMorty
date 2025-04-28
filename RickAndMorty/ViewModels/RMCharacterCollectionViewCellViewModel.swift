f25149cb990dac195005b6f187cdcd16ec1f5147
Razumov Pavel <razumov_pv@mail.ru>
2025-04-28 10:29:22 +0300

    Add ImageLoader

--- RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -10,24 +10,9 @@ import Foundation
 
 final class RMCharacterCollectionViewCellViewModel: Hashable, Equatable {
     
-    
-    
     public let characterName: String
     private let characterStatus: RMCharacterStatus
     private let characterImageUrl: URL?
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(characterName)
-        hasher.combine(characterStatus)
-        hasher.combine(characterImageUrl)
-    }
-    
-    static func == (
-        lhs: RMCharacterCollectionViewCellViewModel,
-        rhs: RMCharacterCollectionViewCellViewModel
-    ) -> Bool {
-        lhs.hashValue == rhs.hashValue
-    }
     
     init(
         characterName: String,
@@ -44,20 +29,25 @@ final class RMCharacterCollectionViewCellViewModel: Hashable, Equatable {
     }
     
     public func fetchImage(completion: @escaping (Result<Data, Error>) -> Void) {
-        //TODO: Abstract to Image Manager
+
         guard let url = characterImageUrl else {
             completion(.failure(URLError(.badURL)))
             return
         }
         
-        let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request) { data, _, error in
-            guard let data, error == nil else {
-                completion(.failure(error ?? URLError(.badServerResponse)))
-                return
-            }
-            completion(.success(data))
-        }
-        task.resume()
+        RMImageLoader.shared.downloadImage(url, completion: completion)
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(characterName)
+        hasher.combine(characterStatus)
+        hasher.combine(characterImageUrl)
+    }
+    
+    static func == (
+        lhs: RMCharacterCollectionViewCellViewModel,
+        rhs: RMCharacterCollectionViewCellViewModel
+    ) -> Bool {
+        lhs.hashValue == rhs.hashValue
     }
 }
